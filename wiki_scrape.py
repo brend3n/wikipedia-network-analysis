5c98e9f478c9b9187def0654dc3df26db31a8a7c
brend3n <brenden@knights.ucf.edu>
2021-01-19 09:16:00 -0500

    Added the remainder of the functionality for auto-adding data to the database instance

--- wiki_scrape.py
+++ wiki_scrape.py
@@ -1,10 +1,17 @@
 from typing import List
 import requests
-
+import config
+from pymongo import MongoClient
 from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+client = config.connect
+db = client.wiki
+
 
 def get_soup(url):
-    print(f"get_soup({url})")
+    # print(f"get_soup({url})")
     headers = {'User-Agent':'Mozilla/5.0'}
     page = requests.get(url, headers=headers)
 
@@ -21,36 +28,87 @@ def get_random_page_url():
 		# print(f"Title: {title}")
 		# print(f"Description: {description}")
 		# print(f"URL: {url}")
+	page_name = title
 
-	return url
+	return url, page_name
+
+def strip_link_prefix(link):
+	link = link[6:]
+	return link
 
 def get_all_links(soup):
 	links = []
 	for link in soup.find_all('a'):
 		l = link.get('href')
-		if "/wiki/" in str(l):
+		l = str(l)
+		if ("/wiki/" in l) and (":" not in l) and (".svg" not in l) and (".org" not in l) and ("Main_Page" not in l):
 			# print(l)
-			links.append(str(l))
+			l = strip_link_prefix(l)
+			links.append(l)
 
 	return links
 
 
 def store(page_name, links):
-	# Store in either: Database (MongoDB) or Excel
-	pass
+	# Store in: Database
+	page_data_model = {
+		"page_name" : page_name,
+		"links" : links,
+		"num_links" : len(links)
+	}
+	try:
+		db.pages.insert_one(page_data_model)
+	except Exception as e:
+		print(f"Error: {e}")
+		print(f"Could not insert link data of {page_name}.")
+		return
+
+	print(f"Successfully added link data of {page_name}.")
+
+def check_if_exists(page_name):
+        try:
+            count = db.pages.count_documents({"page_name":page_name})
+            if count > 0:
+                return True
+            return False
+        except:
+            print("Error in -> check_if_exists() function in wiki_scrape.py")
+
 
 def run():
-	url = get_random_page_url()
+	url, page_name = get_random_page_url()
 	page = get_soup(url)
 	links = get_all_links(page)
-	print(links)
+	# print(links)
+	# print(f'Analyzing: {page_name}')
+	return page_name, links
 
+def clean_links(links):
+	links = list(set(links)) 	
+	# for link in links:
+	# 	print(link)
+	return links
 
+def batch_store():
+	i = 0
+	while True:
+		try:
 
+			ran_page_name, links = run()
 
+			if check_if_exists(ran_page_name):
+				print(f"{ran_page_name} already exists in the database")
+				print("Getting next page...")
+				continue
 
+			links = clean_links(links)
+			store(ran_page_name, links)
+			i+=1
 
-run()
+		except:
+			print("Error has occurred ...")
+			print("Now continuing")
+			batch_store()
 
 
 
